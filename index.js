67d93184a04b799123cfcea8eb38c2634917c97f
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-18 19:55:13 +0100

    "variables completed"

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // this is my first javascript code
 console.log("hello cherie");
 
-let my_name = "CHeeky"; // we cannot use researved keywords here
-console.log(my_name);
+let myName = "CHeeky"; // we cannot use researved keywords here
+console.log(myName);
+
+// cannot be a reserved keyworkd
+// should be meaningful
+// cannot start with a number
+// cannot contain a space or hyphen(-)
+
+let firstName; // camel notation is used as the namin convention for javascript
+// declare mulitiple variables
+// -- the modern best practive is to name them each on a new line
