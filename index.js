264cdde27c5f021c7e115178b1c09346729d3979
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-18 19:58:11 +0100

    constants

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ console.log(myName);
 let firstName; // camel notation is used as the namin convention for javascript
 // declare mulitiple variables
 // -- the modern best practive is to name them each on a new line
+
+// constants
+
+const interestRate = 0.3; // with constants we an create values that cannot be reasigned
+// interestRate = 1; 
+console.log(interestRate);
