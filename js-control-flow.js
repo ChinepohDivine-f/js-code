5ffe7de8ed0a79dddfcef29458f16040269284c3
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-19 13:41:20 +0100

    if , else if and else statements

--- js-control-flow.js
+++ js-control-flow.js
@@ -1,1 +1,11 @@
-console.log('control flow....');
+console.log("control flow....");
+
+// Hour
+// if hour is between 6 am and 12pm: Good moring
+// if it is between 12pm and 6pm: Good afternoon
+// otherwise: Good evening
+
+let hour = 21;
+if (hour >= 6 && hour < 12) console.log("Good morning!");
+else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else console.log("Good evening!");
