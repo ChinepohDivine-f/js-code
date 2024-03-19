eaab476c1480d7dc3b0bdd50424803c1cdce8a8f
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-19 22:10:19 +0100

    factory functions

--- oop.js
+++ oop.js
@@ -6,9 +6,27 @@ const circle = {
   },
   inVisible: true,
   draw: function () {
+    // method
     console.log("Draw");
   },
 };
 
-
 circle.draw();
+
+// Factory functions
+// with these we can create ojects without copy code over and over again
+// creating objects is easier when we use funcion to do so.
+function createCircle(radius) {
+  return {
+    radius, // meeanig the key and value are thesame
+    draw() {
+      console.log("Draw");
+    },
+  };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(3);
+console.log(circle2);
