3069da7344671e5efec86a90ed2b68d75168ec40
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-19 15:59:07 +0100

    max using ternary operator

--- max.js
+++ max.js
@@ -1,7 +1,11 @@
 function max(num1, num2) {
     if (num1 > num2) return num1;
-    else return num2;
+    return num2; 
 }
 
-result = max(3, 4);
+function maxTernary(num1, num2) {
+  return num1 > num2 ?  num1 :  num2
+}
+
+let result = maxTernary(2, 3);
 console.log(result);
