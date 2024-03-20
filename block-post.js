ba98b058c536efb78054709093516a9966086bac
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-20 19:43:48 +0100

    price ranges program for yelp

--- block-post.js
+++ block-post.js
@@ -3,7 +3,8 @@ let post = {
   body: "b",
   author: "c",
   views: 10,
-  comments: [ // array of objects
+  comments: [
+    // array of objects
     {
       author: "a",
       body: "b",
@@ -19,15 +20,13 @@ let post = {
 console.log(post);
 
 function Post(title, body, author) {
-    
-    this.title = "a";
-    this.body = "b";
-    this.author = "c";
-    this.views = 0;
-    this.comments = [];
-    this.isLive = true;
-    
+  this.title = "a";
+  this.body = "b";
+  this.author = "c";
+  this.views = 0;
+  this.comments = [];
+  this.isLive = true;
 }
 
-let newPost = new Post('a', 'b', 'c');
+let newPost = new Post("a", "b", "c");
 console.log(newPost);
---
+++ price-range.js
@@ -0,0 +1,22 @@
+let priceRanges = [
+  {
+    label: "$",
+    tooltip: "Inexpendsive",
+    minPerPerson: 0,
+    maxPerson: 10,
+  },
+  {
+    label: "$",
+    tooltip: "Moderate",
+    minPerPerson: 11,
+    maxPerson: 20,
+  },
+  {
+    label: "$",
+    tooltip: "Expendsive",
+    minPerPerson: 21,
+    maxPerson: 50,
+  },
+];
+
+let restaurants = [{ averagePerPerson: 5 }];
